ebaa5385620b76dbabacb85af59cb2c37ca26e61
poyanpayandeh <poyanpayandeh@hotmail.com>
2022-06-06 07:17:22 +1200

    player movement set up

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ class Sprite {
 
   update() {
     this.draw();
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
     // check if the sprite has reachd the bottom - if so set the velocity to 0 to stop it else velocty.y + gravity
@@ -60,6 +61,20 @@ const enemy = new Sprite({
 
 console.log(player);
 
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  w: {
+    pressed: false,
+  },
+};
+
+let lastKey;
+
 // function to create animation loop
 function animate() {
   window.requestAnimationFrame(animate);
@@ -67,6 +82,42 @@ function animate() {
   c.fillRect(0, 0, canvas.width, canvas.height); // clears canvas before moving sprites
   player.update();
   enemy.update();
+
+  player.velocity.x = 0;
+  if (keys.a.pressed && lastKey === "a") {
+    player.velocity.x = -1;
+  } else if (keys.d.pressed && lastKey === "d") {
+    player.velocity.x = 1;
+  }
 }
 
 animate();
+
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = true;
+      lastKey = "d";
+      break;
+    case "a":
+      keys.a.pressed = true;
+      lastKey = "a";
+      break;
+    case "w":
+      player.velocity.y = -10;
+      break;
+  }
+  console.log(event.key);
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+  }
+  console.log(event.key);
+});
