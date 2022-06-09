48b94522e8af75c5385d0581f6d1e4a77f95192e
poyanpayandeh <poyanpayandeh@hotmail.com>
2022-06-09 23:17:25 +1200

    heheh

--- index.js
+++ index.js
@@ -210,7 +210,7 @@ window.addEventListener("keydown", (event) => {
       enemy.velocity.y = -20;
       break;
     case "ArrowDown":
-      enemy.isAttacking = true;
+      enemy.attack();
       break;
   }
 });
