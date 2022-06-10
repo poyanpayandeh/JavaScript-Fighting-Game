538ae8c70131ce2f0c8e59812b8efbabf64b2a15
poyanpayandeh <poyanpayandeh@hotmail.com>
2022-06-10 14:15:39 +1200

    implemented function for decreasing timer

--- index.js
+++ index.js
@@ -137,6 +137,18 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
   );
 }
 
+//function for decreasing timer
+let timer = 10;
+function decreaseTimer() {
+  setTimeout(decreaseTimer, 1000);
+  if (timer > 0) {
+    timer--;
+    document.querySelector("#timer").innerHTML = timer;
+  }
+}
+
+decreaseTimer();
+
 // function to create animation loop
 function animate() {
   window.requestAnimationFrame(animate);
