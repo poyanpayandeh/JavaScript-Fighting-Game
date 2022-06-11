ecd228b606c361fc7ef31c038824e7b61250fae3
poyanpayandeh <poyanpayandeh@hotmail.com>
2022-06-11 16:25:53 +1200

    refactor code by putting classes in a seperate file from index.js

--- index.js
+++ index.js
@@ -7,70 +7,8 @@ canvas.width = 1024;
 canvas.height = 576;
 c.fillRect(0, 0, canvas.width, canvas.height);
 
-// sprite class
-class Sprite {
-  constructor({ position, velocity, color, offset }) {
-    this.position = position; // sets position of sprite
-    this.velocity = velocity; // sets velocity of sprite
-    this.height = 150;
-    this.width = 50;
-    this.lastKey;
-    this.attackBox = {
-      position: {
-        x: this.position.x,
-        y: this.position.y,
-      },
-      offset: offset,
-      height: 50,
-      width: 100,
-    };
-    this.color = color;
-    this.isAttacking;
-    this.health = 100;
-  }
-
-  // draw funtion to represent sprite
-  draw() {
-    c.fillStyle = this.color;
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-
-    // attack box
-    if (this.isAttacking) {
-      c.fillStyle = "green";
-      c.fillRect(
-        this.attackBox.position.x,
-        this.attackBox.position.y,
-        this.attackBox.width,
-        this.attackBox.height
-      );
-    }
-  }
-
-  update() {
-    this.draw();
-    this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
-    this.attackBox.position.y = this.position.y;
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-
-    // check if the sprite has reached the bottom - if so set the velocity to 0 to stop it else velocty.y + gravity
-    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-      this.velocity.y = 0;
-    } else {
-      this.velocity.y += gravity;
-    }
-  }
-
-  attack() {
-    this.isAttacking = true;
-    setTimeout(() => {
-      this.isAttacking = false;
-    }, 100);
-  }
-}
-
 // creating player 1
-const player = new Sprite({
+const player = new Fighter({
   position: {
     x: 0,
     y: 0,
@@ -87,7 +25,7 @@ const player = new Sprite({
 });
 
 // creating player 2
-const enemy = new Sprite({
+const enemy = new Fighter({
   position: {
     x: 980,
     y: 100,
---
+++ js/classes.js
@@ -0,0 +1,77 @@
+// sprite class
+class Fighter {
+  constructor({ position, velocity, color, offset }) {
+    this.position = position; // sets position of sprite
+    this.velocity = velocity; // sets velocity of sprite
+    this.height = 150;
+    this.width = 50;
+    this.lastKey;
+    this.attackBox = {
+      position: {
+        x: this.position.x,
+        y: this.position.y,
+      },
+      offset: offset,
+      height: 50,
+      width: 100,
+    };
+    this.color = color;
+    this.isAttacking;
+    this.health = 100;
+  }
+
+  // draw funtion to represent sprite
+  draw() {
+    c.fillStyle = this.color;
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    // attack box
+    if (this.isAttacking) {
+      c.fillStyle = "green";
+      c.fillRect(
+        this.attackBox.position.x,
+        this.attackBox.position.y,
+        this.attackBox.width,
+        this.attackBox.height
+      );
+    }
+  }
+
+  update() {
+    this.draw();
+    this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
+    this.attackBox.position.y = this.position.y;
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    // check if the sprite has reached the bottom - if so set the velocity to 0 to stop it else velocty.y + gravity
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
+    }
+  }
+
+  attack() {
+    this.isAttacking = true;
+    setTimeout(() => {
+      this.isAttacking = false;
+    }, 100);
+  }
+}
+
+// sprite class
+class Sprite {
+  constructor({ position }) {
+    this.position = position;
+    this.height = 150;
+    this.width = 50;
+  }
+
+  // draw funtion to represent sprite
+  draw() {}
+
+  update() {
+    this.draw();
+  }
+}
